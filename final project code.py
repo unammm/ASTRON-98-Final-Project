d629f4d2c314664cc6038c4ba480bb31adfc2c8f
unammm <143663258+unammm@users.noreply.github.com>
2023-11-21 13:48:29 -0800

    Add files via upload

---
+++ final project code.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import numpy as np
+import pandas as pd
+df = pd.read_csv('plot (2).tbl(1).csv')
+df
+
+
+# In[7]:
+
+
+y = np.random.randint()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
